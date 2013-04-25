d157ed4e4fed7169009f3082b40b4245cb150042
pfiffnst <pfiffnst@hs-weingarten.de>
2013-04-25 16:02:23 +0200

    Made code PEP8 conform

--- src/bioloidPTU.py
+++ src/bioloidPTU.py
@@ -1,11 +1,15 @@
+#!/usr/bin/env python
+
 import serial
 import math
+import sys
+
 
 # definition of servo ids
-panServoID = 2
-tiltServoID = 6
+PAN_SERVO_ID = 2
+TILT_SERVO_ID = 6
 
-movingSpeed = 100
+MOVING_SPEED = 100
 
 # register addresses (don't change)
 AX12_GOAL_POSITION_L = 30  # set position
@@ -16,49 +20,50 @@ AX_WRITE_DATA = 3
 AX_READ_DATA = 4
 
 
-s = serial.Serial()               # create a serial port object
-s.baudrate = 1000000              # baud rate, in bits/second
-s.port = "/dev/ttyUSB0"           # this is whatever port your are using
+s = serial.Serial()  # create a serial port object
+s.baudrate = 1000000  # baud rate, in bits/second
+s.port = "/dev/ttyUSB0"  # this is whatever port your are using
 s.open()
 
 
 
-def setPanAngleRadian(panAngleRadian):
-    panPosition = int(radianAngleToBioloid(panAngleRadian))
-    setReg(panServoID,AX12_MOVING_SPEED_L,((movingSpeed%256),(movingSpeed>>8)))
-    setReg(panServoID,AX12_GOAL_POSITION_L,((panPosition%256),(panPosition>>8)))
-    
-    print "Set pan angle: " + str(panAngleRadian) + " (bioloid position: " + str(panPosition) + ")"
-    print "Pan servo temperature" + str(getReg(panServoID,43,1)) 
 
-def setTiltAngleRadian(tiltAngleRadian):
-    tiltPosition = int(radianAngleToBioloid(tiltAngleRadian))
-    setReg(tiltServoID,AX12_MOVING_SPEED_L,((movingSpeed%256),(movingSpeed>>8)))
-    setReg(tiltServoID,AX12_GOAL_POSITION_L,((tiltPosition%256),(tiltPosition>>8)))
+def set_pan_angle(pan_angle_radian):
+    panPosition = int(radian_angle_to_bioloid(pan_angle_radian))
+    set_reg(PAN_SERVO_ID, AX12_MOVING_SPEED_L, ((MOVING_SPEED % 256), (MOVING_SPEED >> 8)))
+    set_reg(PAN_SERVO_ID, AX12_GOAL_POSITION_L, ((panPosition % 256), (panPosition >> 8)))
     
-    print "Set tilt angle: " + str(tiltAngleRadian) + " (bioloid position: " + str(tiltPosition) + ")"
+    print "Set pan angle: " + str(pan_angle_radian) + " (bioloid position: " + str(panPosition) + ")"
+    print "Pan servo temperature " + str(get_reg(PAN_SERVO_ID, 43, 1)) 
+
+def set_tilt_angle(tilt_angle_radian):
+    tiltPosition = int(radian_angle_to_bioloid(tilt_angle_radian))
+    set_reg(TILT_SERVO_ID, AX12_MOVING_SPEED_L, ((MOVING_SPEED % 256), (MOVING_SPEED >> 8)))
+    set_reg(TILT_SERVO_ID, AX12_GOAL_POSITION_L, ((tiltPosition % 256), (tiltPosition >> 8)))
     
-    print "Tilt servo temperature " + str(getReg(tiltServoID,43,1))
+    print "Set tilt angle: " + str(tilt_angle_radian) + " (bioloid position: " + str(tiltPosition) + ")"
+    
+    print "Tilt servo temperature " + str(get_reg(TILT_SERVO_ID, 43, 1))
 
 # set register values
-def setReg(ID,reg,values):
+def set_reg(id, reg, values):
     length = 3 + len(values)
-    checksum = 255-((ID+length+AX_WRITE_DATA+reg+sum(values))%256)          
-    s.write(chr(0xFF)+chr(0xFF)+chr(ID)+chr(length)+chr(AX_WRITE_DATA)+chr(reg))
+    checksum = 255 - ((id + length + AX_WRITE_DATA + reg + sum(values)) % 256)          
+    s.write(chr(0xFF) + chr(0xFF) + chr(id) + chr(length) + chr(AX_WRITE_DATA) + chr(reg))
     for val in values:
         s.write(chr(val))
     s.write(chr(checksum))
 
-def getReg(index, regstart, rlength):
+def get_reg(index, regstart, rlength):
     s.flushInput()   
-    checksum = 255 - ((6 + index + regstart + rlength)%256)
-    s.write(chr(0xFF)+chr(0xFF)+chr(index)+chr(0x04)+chr(AX_READ_DATA)+chr(regstart)+chr(rlength)+chr(checksum))
+    checksum = 255 - ((6 + index + regstart + rlength) % 256)
+    s.write(chr(0xFF) + chr(0xFF) + chr(index) + chr(0x04) + chr(AX_READ_DATA) + chr(regstart) + chr(rlength) + chr(checksum))
     vals = list()
-    s.read()   # 0xff
-    s.read()   # 0xff
-    s.read()   # ID
+    s.read()  # 0xff
+    s.read()  # 0xff
+    s.read()  # ID
     length = ord(s.read()) - 1
-    s.read()   # toss error    
+    s.read()  # toss error    
     while length > 0:
         vals.append(ord(s.read()))
         length = length - 1
@@ -66,27 +71,42 @@ def getReg(index, regstart, rlength):
         return vals[0]
     return vals
 
-def bioloidToRadianAngle(bioloidPosition):
-    angleRadian= ((5*math.pi*bioloidPosition)/3066)
-    #Bioloid-Offset: 150 = initial position.
+def bioloid_to_radian_angle(bioloidPosition):
+    angleRadian = ((5 * math.pi * bioloidPosition) / 3066)
+    # Bioloid-Offset: 150 = initial position.
       
-    return angleRadian - (math.pi*5/6);
+    return angleRadian - (math.pi * 5 / 6);
 
-def radianAngleToBioloid(radianAngle):
-    #Bioloid-Offset: 150 = initial position.
-    radianAngle = radianAngle + (math.pi*5/6);
-    return  rRound(((radianAngle/math.pi)*(3066/5)),5)
+def radian_angle_to_bioloid(radianAngle):
+    # Bioloid-Offset: 150 = initial position.
+    radianAngle = radianAngle + (math.pi * 5 / 6);
+    return  r_round(((radianAngle / math.pi) * (3066 / 5)), 5)
 
 
-def rRound(res, dec):
-    for i in range(dec-1, 0 ,-1):
-        tmp = res*math.pow(10, i)
-        if (tmp-math.floor(tmp))>=0.5:
-            res=res+math.pow(10,i*(-1))
+def r_round(res, dec):
+    for i in range(dec - 1, 0 , -1):
+        tmp = res * math.pow(10, i)
+        if (tmp - math.floor(tmp)) >= 0.5:
+            res = res + math.pow(10, i * (-1))
     return res
 
 
-setPanAngleRadian(0) #centered position = 0   #negative values pan right
-setTiltAngleRadian(-0.85) #centered position = 0  #negative values tilt down 
+if __name__ == '__main__':
+    
+    # set default args as -h , if no args:
+    if len(sys.argv) != 3: 
+        print "Usage: bioloidPTU <pan angle in radians> <tilt angle in radians> (default 0 0)"
+        set_pan_angle(0)  # centered position = 0   #negative values pan right
+        set_tilt_angle(0)  # centered position = 0  #negative values tilt down 
+    
+    else:
+        set_pan_angle(float(sys.argv[1]))  # centered position = 0   #negative values pan right
+        set_tilt_angle(float(sys.argv[2]))  # centered position = 0  #negative values tilt down 
+        
+        
+        
+
+   
+
 
 
